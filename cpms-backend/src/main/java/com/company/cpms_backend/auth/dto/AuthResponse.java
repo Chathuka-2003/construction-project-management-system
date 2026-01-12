406655f5af083a41eb7ebd1ee22b590d57f54e04
sandeeparavindi <sandeeparavindi1022@gmail.com>
2026-01-13 00:16:43 +0530

    add auth response

--- cpms-backend/src/main/java/com/company/cpms_backend/auth/dto/AuthResponse.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/auth/dto/AuthResponse.java
@@ -1,4 +1,12 @@
 package com.company.cpms_backend.auth.dto;
 
-public class AuthDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String role;
+    private String email;
 }
