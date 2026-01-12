abc3c200aa5b2e364f498e9c14e456a95b414d9f
sandeeparavindi <sandeeparavindi1022@gmail.com>
2026-01-13 00:15:50 +0530

    login request

--- cpms-backend/src/main/java/com/company/cpms_backend/auth/dto/LoginRequest.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/auth/dto/LoginRequest.java
@@ -1,4 +1,15 @@
 package com.company.cpms_backend.auth.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 }
