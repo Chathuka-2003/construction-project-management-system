f8d6a85b5080964c0b31c44503afda45e90d2949
sandeeparavindi <sandeeparavindi1022@gmail.com>
2026-01-13 00:06:32 +0530

    user repository implement

--- cpms-backend/src/main/java/com/company/cpms_backend/user/repository/UserRepository.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/user/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.company.cpms_backend.user.repository;
 
-public interface UserRepository {
+import com.company.cpms_backend.user.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    Optional<UserModel> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
