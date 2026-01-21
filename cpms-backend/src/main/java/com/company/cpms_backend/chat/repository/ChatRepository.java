267f42ecee5bfe558a30ceff30b882e49d9f9b95
Chathuka Priyankara Edirisinghe <chathukapriyankaraedirisinghe@gmail.com>
2026-01-22 02:01:45 +0530

    created chat repository

--- cpms-backend/src/main/java/com/company/cpms_backend/chat/repository/ChatRepository.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/chat/repository/ChatRepository.java
@@ -1,4 +1,11 @@
 package com.company.cpms_backend.chat.repository;
 
-public interface ChatRepository {
+import com.company.cpms_backend.chat.model.ChatModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ChatRepository extends JpaRepository<ChatModel, Long> {
+
+    List<ChatModel> findAllByProject_IdOrderByTimestampAsc(Long projectId);
 }
