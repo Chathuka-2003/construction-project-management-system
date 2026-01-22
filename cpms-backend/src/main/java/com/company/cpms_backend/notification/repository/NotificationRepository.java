2249eef43a5928512b5f4f60d0eadeaf408e8ade
Chathuka Priyankara Edirisinghe <chathukapriyankaraedirisinghe@gmail.com>
2026-01-23 01:19:11 +0530

    add notification repository

--- cpms-backend/src/main/java/com/company/cpms_backend/notification/repository/NotificationRepository.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/notification/repository/NotificationRepository.java
@@ -1,4 +1,11 @@
 package com.company.cpms_backend.notification.repository;
 
-public interface NotificationRepository {
+import com.company.cpms_backend.notification.model.NotificationModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<NotificationModel, Long> {
+    List<NotificationModel> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
+
