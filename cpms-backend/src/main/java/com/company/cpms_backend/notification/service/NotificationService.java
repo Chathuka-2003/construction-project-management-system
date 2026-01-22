33fe2318c6c6a86144c6c2b8fd189a1474b1da32
Chathuka Priyankara Edirisinghe <chathukapriyankaraedirisinghe@gmail.com>
2026-01-23 01:23:16 +0530

    add notification service

--- cpms-backend/src/main/java/com/company/cpms_backend/notification/service/NotificationService.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/notification/service/NotificationService.java
@@ -1,4 +1,37 @@
 package com.company.cpms_backend.notification.service;
 
+import com.company.cpms_backend.notification.model.NotificationModel;
+import com.company.cpms_backend.notification.repository.NotificationRepository;
+import com.company.cpms_backend.user.model.UserModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public void notify(UserModel user, String title, String message) {
+
+        NotificationModel notification = new NotificationModel();
+        notification.setUser(user);
+        notification.setTitle(title);
+        notification.setMessage(message);
+        notification.setRead(false);
+        notification.setCreatedAt(LocalDateTime.now());
+
+        notificationRepository.save(notification);
+
+        // send realtime notification
+        messagingTemplate.convertAndSendToUser(
+                user.getEmail(),
+                "/queue/notifications",
+                notification
+        );
+    }
 }
