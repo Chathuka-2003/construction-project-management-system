0cdd59587ad79b7a09e024500cbb0fc7018c492b
ttnethmi <tharushikattnts2003@gmail.com>
2026-01-25 14:58:45 +0530

    add router

---
+++ cpms-customer-frontend/src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Gallery from "../pages/Gallery";
+import Login from "../pages/Login";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
