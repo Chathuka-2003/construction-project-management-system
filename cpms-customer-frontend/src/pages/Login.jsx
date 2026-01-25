4c8f9057a41134b8ca211192b6e2108a311dac53
ttnethmi <tharushikattnts2003@gmail.com>
2026-01-25 14:56:31 +0530

    add pages

---
+++ cpms-customer-frontend/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import logo from "../assets/logo.jpeg"; 
+export default function Login() {
+
+  return (
+    <div
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFFBF0",
+        padding: "2rem",
+      }}
+    ></div>
+  );
+}
