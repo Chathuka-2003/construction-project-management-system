12a75bfc568ecec8daaf377a047c6c28a5d2406d
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-01-26 10:57:27 +0530

    add payment create DTO

--- cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PaymentCreateDTO.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PaymentCreateDTO.java
@@ -1,15 +1,13 @@
 package com.company.cpms_backend.payment.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class PaymentCreateDTO {
+    @NotNull
     public Long projectId;
+
+    @NotNull
     public Double amount;
-}
