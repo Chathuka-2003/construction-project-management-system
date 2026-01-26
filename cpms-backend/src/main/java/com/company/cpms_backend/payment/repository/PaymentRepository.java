c1a8ec8f3d7493a3c0a4fd32cfaa849213789bab
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-01-26 15:44:18 +0530

    add payment repository

--- cpms-backend/src/main/java/com/company/cpms_backend/payment/repository/PaymentRepository.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/payment/repository/PaymentRepository.java
@@ -1,4 +1,11 @@
 package com.company.cpms_backend.payment.repository;
 
-public interface PaymentRepository {
-}
+import com.company.cpms_backend.payment.model.PaymentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<PaymentModel, Long> {
+
+    List<PaymentModel> findAllByProject_IdOrderByCreatedAtDesc(Long projectId);
