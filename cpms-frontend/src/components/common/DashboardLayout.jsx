c33f4826a4d7c06c04c443ee379b868003fe1d5d
chamikaWimalasooriya <acdtwimalasooriya@students.nsbm.ac.lk>
2026-01-30 02:19:00 +0530

    add dashboard layout

--- cpms-frontend/src/components/common/DashboardLayout.jsx
+++ cpms-frontend/src/components/common/DashboardLayout.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import Sidebar from "../Sidebar";
-import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Sidebar.jsx";
+import Navbar from "./Navbar.jsx";
 
-export default function DashboardLayout({ role, children }) {
+export default function DashboardLayout({ role }) {
   return (
     <div className="flex h-screen w-full overflow-hidden">
+      {/* Sidebar */}
       <Sidebar role={role} />
 
+      {/* Right side */}
       <div className="flex flex-col flex-1 h-full">
+        {/* Top Navbar */}
         <Navbar role={role} />
+
+        {/* Page content */}
         <main className="flex-1 bg-gray-100 overflow-y-auto p-6">
-          {children}
+          <Outlet />
         </main>
       </div>
     </div>
