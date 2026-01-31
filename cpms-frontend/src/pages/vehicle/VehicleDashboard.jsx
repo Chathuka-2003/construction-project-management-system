ec4e55acbc2211496af5ea1d3f4046c62736c1c9
Suwani Kenulya <[suwanikenulya@gmail.com]>
2026-02-01 03:03:19 +0530

    vehicle dashboard added

--- cpms-frontend/src/pages/vehicle/VehicleDashboard.jsx
+++ cpms-frontend/src/pages/vehicle/VehicleDashboard.jsx
@@ -1,33 +1,34 @@
-import DashboardLayout from "../../components/common/DashboardLayout";
+import { useState } from "react";
+
+
 
 export default function VehicleDashboard() {
-  return (
-    <DashboardLayout role={"admin"}>
-      <main className="p-8">
-        <h1 className="text-2xl font-bold mb-1">
-          Construction Vehicle Management
-        </h1>
-        <p className="text-gray-600 mb-6">
-          Sri Lankan Construction Fleet System
-        </p>
+  const role = localStorage.getItem("role") || "staff"; // "admin" or "staff"
+  const [activeMenu, setActiveMenu] = useState("vehicles");
 
-      <div className="grid grid-cols-4 gap-4">
-        {[
-          ["Total Vehicles", 34],
-          ["Active Vehicles", 32],
-          ["Assigned Vehicles", 26],
-          ["Under Maintenance", 2],
-        ].map(([title, value]) => (
-          <div
-            key={title}
-            className="bg-[#E7D6C1] rounded-xl p-4"
-          >
-            <p className="text-sm">{title}</p>
-            <p className="text-2xl font-bold">{value}</p>
-          </div>
-        ))}
-      </div>
-    </main>
-    </DashboardLayout>
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-12">
+
+
+      {/* Main Content */}
+      <main className="p-8 lg:col-span-10">
+        <h1 className="text-2xl font-bold mb-1">Construction Vehicle Management</h1>
+        <p className="text-gray-600 mb-6">Sri Lankan Construction Fleet System</p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {[
+            ["Total Vehicles", 34],
+            ["Active Vehicles", 32],
+            ["Assigned Vehicles", 26],
+            ["Under Maintenance", 2],
+          ].map(([title, value]) => (
+            <div key={title} className="bg-[#E7D6C1] rounded-xl p-4">
+              <p className="text-sm">{title}</p>
+              <p className="text-2xl font-bold">{value}</p>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
