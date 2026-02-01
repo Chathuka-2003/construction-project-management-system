66656ca55a3f9526e42c9a8874ea7037a3f7ef99
Suwani Kenulya <[suwanikenulya@gmail.com]>
2026-02-01 12:32:41 +0530

    main layout

--- cpms-frontend/src/components/common/MainLayout.jsx
+++ cpms-frontend/src/components/common/MainLayout.jsx
@@ -1,12 +1,8 @@
-// src/components/common/MainLayout.jsx
 import React from "react";
 import AdminLayout from "../../layouts/AdminLayout";
 import StaffLayout from "../../layouts/StaffLayout";
 
-export default function MainLayout({ role, children }) {
-  // Render only the layout for the current role
-  if (role === "admin") {
-    return <AdminLayout>{children}</AdminLayout>;
-  }
-  return <StaffLayout>{children}</StaffLayout>;
+export default function MainLayout({ role }) {
+  if (role === "admin") return <AdminLayout />;
+  return <StaffLayout />;
 }
