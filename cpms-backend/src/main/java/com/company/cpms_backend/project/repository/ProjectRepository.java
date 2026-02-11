540fe9d06fc4f060983dac04ea8056f0f0ea0729
suvinyajayali426-max <suvinyajayali426@gmail.com>
2026-02-11 23:59:15 +0530

    complete project repo

--- cpms-backend/src/main/java/com/company/cpms_backend/project/repository/ProjectRepository.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/project/repository/ProjectRepository.java
@@ -1,4 +1,12 @@
 package com.company.cpms_backend.project.repository;
 
-public interface ProjectRepository {
+import com.company.cpms_backend.project.model.ProjectModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectRepository extends JpaRepository<ProjectModel, Long> {
+    List<ProjectModel> findByCustomer_Id(Long customerId);
+    List<ProjectModel> findByManager_Id(Long managerId);
+    List<ProjectModel> findAllByCustomerIsNotNull();
 }
