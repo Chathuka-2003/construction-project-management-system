a22dc9588829e34a30e64c82838c378e453e9644
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-02-13 13:48:55 +0530

    update PaymentStatusUpdateDTO

--- cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PaymentStatusUpdateDTO.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PaymentStatusUpdateDTO.java
@@ -1,4 +1,13 @@
 package com.company.cpms_backend.payment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentStatusUpdateDTO {
+    @NotBlank
+    private String status; // "PENDING" | "PAID" | "FAILED"
 }
