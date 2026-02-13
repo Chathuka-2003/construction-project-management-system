3ce09788f37aad1969555e8207d9320951fb4915
R.W.A.K.H Rajasekara <kavishirajasekara@gmail.com>
2026-02-13 13:41:53 +0530

    update PayHereNotifyRequest

--- cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PayHereNotifyRequest.java
+++ cpms-backend/src/main/java/com/company/cpms_backend/payment/dto/PayHereNotifyRequest.java
@@ -4,19 +4,12 @@ import lombok.Data;
 
 @Data
 public class PayHereNotifyRequest {
-
     private String merchant_id;
-
     private String order_id;
-
     private String payment_id;
-
     private String payhere_amount;
-
     private String payhere_currency;
-
     private String status_code;
-
     private String md5sig;
 }
 
