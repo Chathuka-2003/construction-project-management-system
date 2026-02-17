a4b1c82209ab73d486625d83ae28c6bad93e3011
sharadi2002 <sharadisachini86@gmail.com>
2026-02-17 15:16:39 +0530

    updated vehicle assignment

--- cpms-frontend/src/pages/vehicle/VehicleAssignments.jsx
+++ cpms-frontend/src/pages/vehicle/VehicleAssignments.jsx
@@ -12,7 +12,7 @@ import {
 } from "lucide-react";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-
+//change vehicle assignment 
 const api = axios.create({ baseURL: BASE_URL });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
